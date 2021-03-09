9b160a2e531d45385872b6f37081a50bea5e2a2d
jorejia <lijia12@lzu.edu.cn>
2021-03-09 12:16:52 +0800

    update

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,6 +2,7 @@
 opkg update
 opkg install zstd
 opkg install libzstd
+mkdir /tmp/update
 cd /tmp/update
 rm -rf artifact openwrt-rockchip*.img.gz openwrt-rockchip*img*
 wget https://github.com/jorejia/NanoPi-R2S-2021/releases/download/$(date +%Y.%m.%d)-Lean/openwrt-rockchip-armv8-friendlyarm_nanopi-r2s-squashfs-sysupgrade.img.gz
