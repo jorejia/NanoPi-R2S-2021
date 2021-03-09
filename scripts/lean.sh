20d81cfb1b98f135d2bef868e6b1361b8e9c9c58
jorejia <lijia12@lzu.edu.cn>
2021-03-09 21:06:07 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -6,6 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 
+# Add luci-app-koolproxyR
+pushd package
+git clone --depth=1 https://github.com/jefferymvp/luci-app-koolproxyR
+popd
+
 # Add luci-app-ssr-plus
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
@@ -17,9 +22,6 @@ pushd package/community
 
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
-
-# Add luci-app-koolproxyR
-git clone --depth=1 https://github.com/jefferymvp/luci-app-koolproxyR
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
