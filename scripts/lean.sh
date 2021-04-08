0d8aed0ec450a3401c4883d34b4636fe2171323a
jorejia <lijia12@lzu.edu.cn>
2021-04-09 00:51:55 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -45,7 +45,8 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
-svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+svn co https://github.com/281677160/openwrt-package/branches/19.07/luci-app-adguardhome
+svn co https://github.com/281677160/openwrt-package/branches/19.07/adguardhome
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
