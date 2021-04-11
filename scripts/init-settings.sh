7b664918106a6341b680a03cca7edc951d4d638a
jorejia <lijia12@lzu.edu.cn>
2021-04-12 02:31:41 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -18,12 +18,5 @@ sed -i 's/releases\/18.06.8/snapshots/g' /etc/opkg/distfeeds.conf
 # Disable autostart by default for some packages
 cd /etc/rc.d
 rm -f S98udptools || true
-rm -f S99nft-qos || true
-
-# Try to execute init.sh (if exists)
-
-if [ ! -f "/boot/init.sh" ]; then
-bash /boot/init.sh
-fi
 
 exit 0
